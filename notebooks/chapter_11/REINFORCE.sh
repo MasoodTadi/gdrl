36f997e275593b32bbaf4b921c7f543b9ad13d55
Masood <m.tadi.ie@gmail.com>
2024-09-01 15:55:03 +0200

    edit

--- notebooks/chapter_11/REINFORCE.sh
+++ notebooks/chapter_11/REINFORCE.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=REINFORCE        # Job name
-#SBATCH --ntasks=1                  # Number of tasks (usually 1 for single-node jobs)
-#SBATCH --cpus-per-task=128         # Number of CPU cores per task
-#SBATCH --mem=128G                  # Amount of memory (RAM) needed
-#SBATCH --time=01:00:00             # Maximum run time (hh:mm:ss)
-#SBATCH --output=REINFORCE.out      # File to write standard output
-#SBATCH --error=REINFORCE.err       # File to write standard error
+#SBATCH --job-name=REINFORCE
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64         # Reduce CPU cores if needed
+#SBATCH --mem=64G                  # Reduce memory if needed
+#SBATCH --time=01:00:00            # Adjust time as required
+#SBATCH --output=REINFORCE.out
+#SBATCH --error=REINFORCE.err
 
 # Change to the directory where your script is located
 cd ~//gdrl//notebooks//chapter_11//
 
-# Print the current working directory (optional, for debugging)
+# Print the current working directory (optional)
 pwd
 
-# Load the necessary Python module
-module load python/3.8   # Adjust the Python version as needed
+# Load the necessary module
+module load python/3.8
 
 # Run the Python script
 time python REINFORCE.py
