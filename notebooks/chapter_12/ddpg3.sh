75404c04313433db3a0150f8cd4e5e3b7a8140f2
Masood <m.tadi.ie@gmail.com>
2024-11-01 16:46:27 +0100

    Add file ddpg3.sh

---
+++ notebooks/chapter_12/ddpg3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -N ddpg3
+#PBS -l select=1:ncpus=16:mem=32gb
+#PBS -l walltime=72:00:00
+#PBS -o ddpg3.o
+#PBS -e ddpg3.e
+
+# Print the current working directory (optional)
+pwd
+
+# Load the necessary module
+#module load python/3.8
+
+# Activate the virtual environment
+source /storage/praha1/home/tadim/myenv/bin/activate
+
+# Set PYTHONPATH to include local packages
+#export PYTHONPATH=$PYTHONPATH:/storage/praha1/home/tadim/.local/lib/python3.8/site-packages
+
+# Change to the working directory
+cd /storage/praha1/home/tadim/gdrl/notebooks/chapter_12
+
+# Run the Python script
+time python -u ddpg3.py
+
