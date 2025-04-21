b4021f55cf1c6a4afc287db80c203d9136b1c2a4
Masood <m.tadi.ie@gmail.com>
2025-04-21 12:50:34 +0200

    added file

---
+++ notebooks/chapter_12/ppo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -N ppo
+#PBS -l select=1:ncpus=16:mem=32gb
+#PBS -l walltime=12:00:00
+#PBS -o ppo.o
+#PBS -e ppo.e
+
+# Print the current working directory (optional)
+pwd
+
+# Load the necessary module
+#module load python/3.8
+
+# Activate the virtual environment
+source /storage/praha1/home/tadim/myenv/bin/activate
+
+# Set PYTHONPATH to include local packages
+#export PYTHONPATH=$PYTHONPATH:/storage/praha1/home/tadim/.local/lib/python3.8/site-packages
+
+# Change to the working directory
+cd /storage/praha1/home/tadim/gdrl/notebooks/chapter_12
+
+# Run the Python script
+time python -u ppo.py
