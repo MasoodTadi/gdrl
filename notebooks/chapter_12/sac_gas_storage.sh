8491c5a23558f8c2952be814938def9cf67730ec
Masood <m.tadi.ie@gmail.com>
2025-05-03 16:28:02 +0200

    added file

---
+++ notebooks/chapter_12/sac_gas_storage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -N sac_gas_storage
+#PBS -l select=1:ncpus=4:mem=32gb 
+#PBS -l walltime=96:00:00
+#PBS -o sac_gas_storage.o
+#PBS -e sac_gas_storage.e
+
+# Print the current working directory (optional)
+pwd
+
+# Load the necessary module
+#module load python/3.8
+
+# Activate the virtual environment
+source /storage/praha1/home/tadim/myenv/bin/activate
+
+# Set PYTHONPATH to include local packages
+#export PYTHONPATH=$PYTHONPATH:/storage/praha1/home/tadim/.local/lib/python3.8/site-packages
+
+# Change to the working directory
+cd /storage/praha1/home/tadim/gdrl/notebooks/chapter_12
+
+# Now loop over values
+time python -u sac_gas_storage.py | tee output_sac_gas_storage.log
