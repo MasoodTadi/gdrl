d8b2e7efb4ec0135863533115d7add05ebc6dc4a
Masood <m.tadi.ie@gmail.com>
2025-05-19 16:50:25 +0200

    fix error

--- notebooks/chapter_12/rolling_intrinsic_penalized_autoregressive.py
+++ notebooks/chapter_12/rolling_intrinsic_penalized_autoregressive.py
@@ -1070,7 +1070,7 @@ for seed in SEEDS:
         'env_name': 'TTFGasStorageEnv',
         'gamma': 1.0,
         'max_minutes': np.inf,#20,
-        'max_episodes': 100,
+        'max_episodes': 50_000,
         'goal_mean_100_reward': 4.5#-15#-150
     }
 
