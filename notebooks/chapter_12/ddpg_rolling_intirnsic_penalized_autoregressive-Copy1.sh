aa9b07dc3dc27872f8cd0abc5f2c1dca65168396
Masood <m.tadi.ie@gmail.com>
2025-05-19 16:52:41 +0200

    file added

---
+++ notebooks/chapter_12/ddpg_rolling_intirnsic_penalized_autoregressive-Copy1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -N rolling_intrinsic_penalized_autoregressive
+#PBS -l select=1:ncpus=16:mem=32gb
+#PBS -l walltime=96:00:00
+#PBS -o rolling_intrinsic_penalized_autoregressive.o
+#PBS -e rolling_intrinsic_penalized_autoregressive.e
+
+# Print the current working directory (optional)
+pwd
+
+# Load the necessary module
+#module load python/3.8
+
+# Activate the virtual environment
+source /storage/praha1/home/tadim/myenv/bin/activate
+
+# Set PYTHONPATH to include local packages
+#export PYTHONPATH=$PYTHONPATH:/storage/praha1/home/tadim/.local/lib/python3.8/site-packages
+
+# Change to the working directory
+cd /storage/praha1/home/tadim/gdrl/notebooks/chapter_12
+
+# Run the Python script
+# time python -u autoregressive.py
+# time python -u autoregressive.py > output_autoregressive.log 2>&1
+time python -u rolling_intrinsic_penalized_autoregressive.py | tee output_rolling_intrinsic_penalized_autoregressive.log
