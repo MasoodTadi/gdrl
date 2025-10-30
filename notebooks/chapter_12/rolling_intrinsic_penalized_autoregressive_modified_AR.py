d51eece099e3b6d1c02a3a6a5eaf406568180434
Masood <m.tadi.ie@gmail.com>
2025-10-30 14:13:46 +0100

    'goal_mean_100_reward': 3.8
    SEEDS = (
        12, 34, 56, 78, 90, 123, 145, 167, 189, 210,
        256, 312, 478, 512, 634, 758, 890, 912, 1024, 2048
    )

--- notebooks/chapter_12/rolling_intrinsic_penalized_autoregressive_modified_AR.py
+++ notebooks/chapter_12/rolling_intrinsic_penalized_autoregressive_modified_AR.py
@@ -1163,8 +1163,12 @@ class NormalNoiseStrategy:
         # return final_action
         return action
 
-SEEDS = (34, 56, 78, 90)
+# SEEDS = (34, 56, 78, 90)
 #SEEDS = (56, 78, 90)
+SEEDS = (
+    12, 34, 56, 78, 90, 123, 145, 167, 189, 210,
+    256, 312, 478, 512, 634, 758, 890, 912, 1024, 2048
+)
 SEEDS = [90]
 ddpg_results = []
 best_agent, best_eval_score = None, float('-inf')
@@ -1173,8 +1177,8 @@ for seed in SEEDS:
         'env_name': 'TTFGasStorageEnv',
         'gamma': 0.99,#1.0,
         'max_minutes': np.inf,#20,
-        'max_episodes': 50_000, #20_000, #15_000,
-        'goal_mean_100_reward': np.inf#4.1#-15#-150
+        'max_episodes': 20_000, #15_000,
+        'goal_mean_100_reward': 3.8#np.inf#4.1#-15#-150
     }
 
     # policy_model_fn = lambda nS, bounds: FCDPAutoregressive(nS, bounds, hidden_dims=(256,256)) 
