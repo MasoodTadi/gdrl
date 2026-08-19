928ac98cac6f95b09265dd57c21b7bc1ee5275ad
MasoodTadi <m.tadi.ie@gmail.com>
2026-08-19 17:57:40 +0200

    fix errors

--- notebooks/chapter_12/rolling_intrinsic_AR3_physical.sh
+++ notebooks/chapter_12/rolling_intrinsic_AR3_physical.sh
@@ -5,22 +5,24 @@
 #PBS -o rolling_intrinsic_AR3_physical.o
 #PBS -e rolling_intrinsic_AR3_physical.e
 
-# Print the current working directory (optional)
-pwd
-
-# Load the necessary module
-#module load python/3.8
-
-# Activate the virtual environment
-source /storage/praha1/home/tadim/myenv/bin/activate
-
-# Set PYTHONPATH to include local packages
-#export PYTHONPATH=$PYTHONPATH:/storage/praha1/home/tadim/.local/lib/python3.8/site-packages
-
-# Change to the working directory
+# Go to working directory
 cd /storage/praha1/home/tadim/gdrl/notebooks/chapter_12
 
-# Run the Python script
-# time python -u autoregressive.py
-# time python -u autoregressive.py > output_autoregressive.log 2>&1
-time python -u rolling_intrinsic_AR3_physical.py | tee output_rolling_intrinsic_AR3_physical.log
+# Load Mamba
+module add mambaforge
+
+# Activate the correct environment
+mamba activate myenv
+
+# Debug information
+echo "Python being used:"
+which python
+
+echo "Python version:"
+python --version
+
+echo "PyTorch test:"
+python -c "import torch; print('Torch:', torch.__version__)"
+
+# Run program
+time python -u rolling_intrinsic_AR3_physical.py 2>&1 | tee output_rolling_intrinsic_AR3_physical.log
